91806b95188abdb419cabcee9224b4336795422e
Mara Bos <m-ou.se@m-ou.se>
2020-08-07 13:25:08 +0200

    Formatting.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Floating point conversion functions.
 //!
-//! ## Emulated and Native Conversions
+//! ## Emulated and native conversions
 //!
 //! The [`native`] module provides only those functions which are natively available on the target.
 //!
@@ -26,49 +26,68 @@ pub mod emulated;
 /// **Please note:** The contents of this module varies by target.
 /// Functions available on one target might not exist on another.
 pub mod native {
-    #[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86", target_feature = "vfp2"))]
-    pub fn u32_to_f64(x: u32) -> f64 { x as f64 }
+    #[cfg(any(
+        target_arch = "aarch64",
+        target_arch = "x86_64",
+        target_arch = "x86",
+        target_feature = "vfp2"
+    ))]
+    pub fn u32_to_f64(x: u32) -> f64 {
+        x as f64
+    }
 
     #[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86"))]
-    pub fn u64_to_f64_round(x: u64) -> f64 { x as f64 }
+    pub fn u64_to_f64_round(x: u64) -> f64 {
+        x as f64
+    }
 
-    #[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86", target_feature = "vfp2"))]
-    pub fn i32_to_f64(x: i32) -> f64 { x as f64 }
+    #[cfg(any(
+        target_arch = "aarch64",
+        target_arch = "x86_64",
+        target_arch = "x86",
+        target_feature = "vfp2"
+    ))]
+    pub fn i32_to_f64(x: i32) -> f64 {
+        x as f64
+    }
 
     #[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86"))]
-    pub fn i64_to_f64_round(x: i64) -> f64 { x as f64 }
+    pub fn i64_to_f64_round(x: i64) -> f64 {
+        x as f64
+    }
 }
 
 pub use emulated::{
-    i128_to_f64_round,
-    i128_to_f64_truncate,
-    u128_to_f64_round,
-    u128_to_f64_truncate,
-    u64_to_f64_truncate,
-    i64_to_f64_truncate,
-    i128_to_f64_truncate as i128_to_f64_any,
-    u128_to_f64_truncate as u128_to_f64_any,
+    i128_to_f64_round, i128_to_f64_truncate, i128_to_f64_truncate as i128_to_f64_any,
+    i64_to_f64_truncate, u128_to_f64_round, u128_to_f64_truncate,
+    u128_to_f64_truncate as u128_to_f64_any, u64_to_f64_truncate,
 };
 
-#[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86", target_feature = "vfp2"))]
+#[cfg(any(
+    target_arch = "aarch64",
+    target_arch = "x86_64",
+    target_arch = "x86",
+    target_feature = "vfp2"
+))]
 pub use native::{i32_to_f64, u32_to_f64};
 
 #[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86"))]
 pub use native::{
-    i64_to_f64_round,
-    u64_to_f64_round,
-    i64_to_f64_round as i64_to_f64_any,
+    i64_to_f64_round, i64_to_f64_round as i64_to_f64_any, u64_to_f64_round,
     u64_to_f64_round as u64_to_f64_any,
 };
 
-#[cfg(not(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86", target_feature = "vfp2")))]
+#[cfg(not(any(
+    target_arch = "aarch64",
+    target_arch = "x86_64",
+    target_arch = "x86",
+    target_feature = "vfp2"
+)))]
 pub use emulated::{i32_to_f64, u32_to_f64};
 
 #[cfg(not(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86")))]
 pub use emulated::{
-    i64_to_f64_round,
-    u64_to_f64_round,
-    i64_to_f64_truncate as i64_to_f64_any,
+    i64_to_f64_round, i64_to_f64_truncate as i64_to_f64_any, u64_to_f64_round,
     u64_to_f64_truncate as u64_to_f64_any,
 };
 
