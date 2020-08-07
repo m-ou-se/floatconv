7a7d16fc0b374316347ca1d6abe70a1cebe84d61
Mara Bos <m-ou.se@m-ou.se>
2020-08-07 13:13:44 +0200

    Add documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
+//! Floating point conversion functions.
+//!
+//! ## Emulated and Native Conversions
+//!
+//! The [`native`] module provides only those functions which are natively available on the target.
+//!
+//! The [`emulated`] module provides an emulated implementation of all
+//! conversion functions, for targets which do not provide them natively.
+//!
+//! The root of the crate exposes the best version of all available functions.
+//!
+//! ## Rounding mode
+//!
+//! - Functions named `_round` round the integer to the closest possible floating point number, and breaks ties to even.
+//! - Functions named `_truncate` truncate the result, which means they round towards zero.
+//! - Functions without a rounding mode in their name do not round. These conversions are always lossless.
+
+/// Floating point conversion functions emulated without floating point instructions.
+///
+/// Available on all platforms.
 pub mod emulated;
 
+/// Floating point conversion instructions natively available on the target.
+///
+/// **Please note:** The contents of this module varies by target.
+/// Functions available on one target might not exist on another.
 pub mod native {
     #[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86", target_feature = "vfp2"))]
     pub fn u32_to_f64(x: u32) -> f64 { x as f64 }
