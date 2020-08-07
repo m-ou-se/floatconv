03d614e40b3e4cddb0149b132a7f1aa73dc1c109
Mara Bos <m-ou.se@m-ou.se>
2020-08-07 19:45:29 +0200

    Simplify benchmark.

--- benchmark/benchmark.rs
+++ benchmark/benchmark.rs
@@ -3,14 +3,13 @@ use floatconv::*;
 
 fn bench_u128_to_f64(c: &mut Criterion) {
     let mut group = c.benchmark_group("u128_to_f64");
-    for input in &[0, 1234u128, 1234u128 << 80] {
-        group.bench_with_input(BenchmarkId::new("floatconv", input), input, |b, &x| {
-            b.iter(|| u128_to_f64_round(black_box(x)))
-        });
-        group.bench_with_input(BenchmarkId::new("builtin", input), input, |b, &x| {
-            b.iter(|| black_box(x) as f64)
-        });
-    }
+    let input = 1234u128 << 80;
+    group.bench_with_input(BenchmarkId::new("floatconv", input), &input, |b, &x| {
+        b.iter(|| u128_to_f64_round(black_box(x)))
+    });
+    group.bench_with_input(BenchmarkId::new("builtin", input), &input, |b, &x| {
+        b.iter(|| black_box(x) as f64)
+    });
     group.finish();
 }
 
