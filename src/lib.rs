5973c8890ce436b531d4cf74f8d49cab23b59268
Mara Bos <m-ou.se@m-ou.se>
2020-08-07 12:55:52 +0200

    Add native instructions for x86 and arm.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 pub mod emulated;
 
 pub mod native {
-    #[cfg(any(target_arch = "aarch64", target_arch = "x86_64"))]
+    #[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86", target_feature = "vfp2"))]
     pub fn u32_to_f64(x: u32) -> f64 { x as f64 }
 
-    #[cfg(any(target_arch = "aarch64", target_arch = "x86_64"))]
+    #[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86"))]
     pub fn u64_to_f64_round(x: u64) -> f64 { x as f64 }
 
-    #[cfg(any(target_arch = "aarch64", target_arch = "x86_64"))]
+    #[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86", target_feature = "vfp2"))]
     pub fn i32_to_f64(x: i32) -> f64 { x as f64 }
 
-    #[cfg(any(target_arch = "aarch64", target_arch = "x86_64"))]
+    #[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86"))]
     pub fn i64_to_f64_round(x: i64) -> f64 { x as f64 }
 }
 
@@ -19,11 +19,17 @@ pub use emulated::{
     u64_to_f64_truncate,
 };
 
-#[cfg(not(any(target_arch = "aarch64", target_arch = "x86_64")))]
-pub use emulated::{i32_to_f64, i64_to_f64_round, u32_to_f64, u64_to_f64_round};
+#[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86", target_feature = "vfp2"))]
+pub use native::{i32_to_f64, u32_to_f64};
 
-#[cfg(any(target_arch = "aarch64", target_arch = "x86_64"))]
-pub use native::*;
+#[cfg(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86"))]
+pub use native::{i64_to_f64_round, u64_to_f64_round};
+
+#[cfg(not(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86", target_feature = "vfp2")))]
+pub use emulated::{i32_to_f64, u32_to_f64};
+
+#[cfg(not(any(target_arch = "aarch64", target_arch = "x86_64", target_arch = "x86")))]
+pub use emulated::{i64_to_f64_round, u64_to_f64_round};
 
 #[cfg(test)]
 mod test;
