9f61dcc5784570cdf5c5d7f5176540f7b0f4a7ab
Mara Bos <m-ou.se@m-ou.se>
2020-08-09 21:32:34 +0200

    Fix name of 8-bit and 16-bit conversions.
    They do not round. They are lossless.

--- src/fast/mod.rs
+++ src/fast/mod.rs
@@ -23,20 +23,20 @@ impl_soft!(i128_to_f64_truncate i128 f64);
 
 #[cfg(target_arch = "aarch64")]
 group! {
-    impl_native!(u8_to_f32_round u8 f32);
-    impl_native!(i8_to_f32_round i8 f32);
-    impl_native!(u16_to_f32_round u16 f32);
-    impl_native!(i16_to_f32_round i16 f32);
+    impl_native!(u8_to_f32 u8 f32);
+    impl_native!(i8_to_f32 i8 f32);
+    impl_native!(u16_to_f32 u16 f32);
+    impl_native!(i16_to_f32 i16 f32);
     impl_native!(u32_to_f32_round u32 f32);
     impl_native!(i32_to_f32_round i32 f32);
     impl_native!(u64_to_f32_round u64 f32);
     impl_native!(i64_to_f32_round i64 f32);
     impl_soft!(u128_to_f32_round u128 f32);
     impl_soft!(i128_to_f32_round i128 f32);
-    impl_native!(u8_to_f64_round u8 f64);
-    impl_native!(i8_to_f64_round i8 f64);
-    impl_native!(u16_to_f64_round u16 f64);
-    impl_native!(i16_to_f64_round i16 f64);
+    impl_native!(u8_to_f64 u8 f64);
+    impl_native!(i8_to_f64 i8 f64);
+    impl_native!(u16_to_f64 u16 f64);
+    impl_native!(i16_to_f64 i16 f64);
     impl_native!(u32_to_f64 u32 f64);
     impl_native!(i32_to_f64 i32 f64);
     impl_native!(u64_to_f64_round u64 f64);
@@ -47,20 +47,20 @@ group! {
 
 #[cfg(target_arch = "x86_64")]
 group! {
-    impl_native!(u8_to_f32_round u8 f32);
-    impl_native!(i8_to_f32_round i8 f32);
-    impl_native!(u16_to_f32_round u16 f32);
-    impl_native!(i16_to_f32_round i16 f32);
+    impl_native!(u8_to_f32 u8 f32);
+    impl_native!(i8_to_f32 i8 f32);
+    impl_native!(u16_to_f32 u16 f32);
+    impl_native!(i16_to_f32 i16 f32);
     impl_native!(u32_to_f32_round u32 f32);
     impl_native!(i32_to_f32_round i32 f32);
     impl_special!(u64_to_f32_round u64 f32);
     impl_native!(i64_to_f32_round i64 f32);
     impl_soft!(u128_to_f32_round u128 f32);
     impl_soft!(i128_to_f32_round i128 f32);
-    impl_native!(u8_to_f64_round u8 f64);
-    impl_native!(i8_to_f64_round i8 f64);
-    impl_native!(u16_to_f64_round u16 f64);
-    impl_native!(i16_to_f64_round i16 f64);
+    impl_native!(u8_to_f64 u8 f64);
+    impl_native!(i8_to_f64 i8 f64);
+    impl_native!(u16_to_f64 u16 f64);
+    impl_native!(i16_to_f64 i16 f64);
     impl_native!(u32_to_f64 u32 f64);
     impl_native!(i32_to_f64 i32 f64);
     impl_special!(u64_to_f64_round u64 f64);
@@ -71,20 +71,20 @@ group! {
 
 #[cfg(all(target_arch = "x86", target_feature = "sse2"))]
 group! {
-    impl_native!(u8_to_f32_round u8 f32);
-    impl_native!(i8_to_f32_round i8 f32);
-    impl_native!(u16_to_f32_round u16 f32);
-    impl_native!(i16_to_f32_round i16 f32);
+    impl_native!(u8_to_f32 u8 f32);
+    impl_native!(i8_to_f32 i8 f32);
+    impl_native!(u16_to_f32 u16 f32);
+    impl_native!(i16_to_f32 i16 f32);
     impl_native!(u32_to_f32_round u32 f32);
     impl_native!(i32_to_f32_round i32 f32);
     impl_special!(u64_to_f32_round u64 f32);
     impl_native!(i64_to_f32_round i64 f32);
     impl_soft!(u128_to_f32_round u128 f32);
     impl_soft!(i128_to_f32_round i128 f32);
-    impl_native!(u8_to_f64_round u8 f64);
-    impl_native!(i8_to_f64_round i8 f64);
-    impl_native!(u16_to_f64_round u16 f64);
-    impl_native!(i16_to_f64_round i16 f64);
+    impl_native!(u8_to_f64 u8 f64);
+    impl_native!(i8_to_f64 i8 f64);
+    impl_native!(u16_to_f64 u16 f64);
+    impl_native!(i16_to_f64 i16 f64);
     impl_native!(u32_to_f64 u32 f64);
     impl_native!(i32_to_f64 i32 f64);
     impl_special!(u64_to_f64_round u64 f64);
@@ -95,20 +95,20 @@ group! {
 
 #[cfg(all(target_arch = "x86", not(target_feature = "sse2")))]
 group! {
-    impl_native!(u8_to_f32_round u8 f32);
-    impl_native!(i8_to_f32_round i8 f32);
-    impl_native!(u16_to_f32_round u16 f32);
-    impl_native!(i16_to_f32_round i16 f32);
+    impl_native!(u8_to_f32 u8 f32);
+    impl_native!(i8_to_f32 i8 f32);
+    impl_native!(u16_to_f32 u16 f32);
+    impl_native!(i16_to_f32 i16 f32);
     impl_special!(u32_to_f32_round u32 f32);
     impl_native!(i32_to_f32_round i32 f32);
     impl_soft!(u64_to_f32_round u64 f32);
     impl_soft!(i64_to_f32_round i64 f32);
     impl_soft!(u128_to_f32_round u128 f32);
     impl_soft!(i128_to_f32_round i128 f32);
-    impl_native!(u8_to_f64_round u8 f64);
-    impl_native!(i8_to_f64_round i8 f64);
-    impl_native!(u16_to_f64_round u16 f64);
-    impl_native!(i16_to_f64_round i16 f64);
+    impl_native!(u8_to_f64 u8 f64);
+    impl_native!(i8_to_f64 i8 f64);
+    impl_native!(u16_to_f64 u16 f64);
+    impl_native!(i16_to_f64 i16 f64);
     impl_native!(u32_to_f64 u32 f64);
     impl_native!(i32_to_f64 i32 f64);
     impl_soft!(u64_to_f64_round u64 f64);
@@ -119,20 +119,20 @@ group! {
 
 #[cfg(target_feature = "vfp2")]
 group! {
-    impl_native!(u8_to_f32_round u8 f32);
-    impl_native!(i8_to_f32_round i8 f32);
-    impl_native!(u16_to_f32_round u16 f32);
-    impl_native!(i16_to_f32_round i16 f32);
+    impl_native!(u8_to_f32 u8 f32);
+    impl_native!(i8_to_f32 i8 f32);
+    impl_native!(u16_to_f32 u16 f32);
+    impl_native!(i16_to_f32 i16 f32);
     impl_native!(u32_to_f32_round u32 f32);
     impl_native!(i32_to_f32_round i32 f32);
     impl_soft!(u64_to_f32_round u64 f32);
     impl_soft!(i64_to_f32_round i64 f32);
     impl_soft!(u128_to_f32_round u128 f32);
     impl_soft!(i128_to_f32_round i128 f32);
-    impl_native!(u8_to_f64_round u8 f64);
-    impl_native!(i8_to_f64_round i8 f64);
-    impl_native!(u16_to_f64_round u16 f64);
-    impl_native!(i16_to_f64_round i16 f64);
+    impl_native!(u8_to_f64 u8 f64);
+    impl_native!(i8_to_f64 i8 f64);
+    impl_native!(u16_to_f64 u16 f64);
+    impl_native!(i16_to_f64 i16 f64);
     impl_native!(u32_to_f64 u32 f64);
     impl_native!(i32_to_f64 i32 f64);
     impl_soft!(u64_to_f64_round u64 f64);
@@ -148,20 +148,20 @@ group! {
     target_feature = "vfp2",
 )))]
 group! {
-    impl_soft!(u8_to_f32_round u8 f32);
-    impl_soft!(i8_to_f32_round i8 f32);
-    impl_soft!(u16_to_f32_round u16 f32);
-    impl_soft!(i16_to_f32_round i16 f32);
+    impl_soft!(u8_to_f32 u8 f32);
+    impl_soft!(i8_to_f32 i8 f32);
+    impl_soft!(u16_to_f32 u16 f32);
+    impl_soft!(i16_to_f32 i16 f32);
     impl_soft!(u32_to_f32_round u32 f32);
     impl_soft!(i32_to_f32_round i32 f32);
     impl_soft!(u64_to_f32_round u64 f32);
     impl_soft!(i64_to_f32_round i64 f32);
     impl_soft!(u128_to_f32_round u128 f32);
     impl_soft!(i128_to_f32_round i128 f32);
-    impl_soft!(u8_to_f64_round u8 f64);
-    impl_soft!(i8_to_f64_round i8 f64);
-    impl_soft!(u16_to_f64_round u16 f64);
-    impl_soft!(i16_to_f64_round i16 f64);
+    impl_soft!(u8_to_f64 u8 f64);
+    impl_soft!(i8_to_f64 i8 f64);
+    impl_soft!(u16_to_f64 u16 f64);
+    impl_soft!(i16_to_f64 i16 f64);
     impl_soft!(u32_to_f64 u32 f64);
     impl_soft!(i32_to_f64 i32 f64);
     impl_soft!(u64_to_f64_round i64 f64);
