0db16fd99edc68b7f1b2da7d355acfdcd6ea3bde
Mara Bos <m-ou.se@m-ou.se>
2020-08-12 01:08:59 +0200

    Add soft implementations of conversions to integer.

--- src/soft.rs
+++ src/soft.rs
@@ -4,8 +4,8 @@
 //! Available on all platforms.
 //!
 //! To avoid using any floating point instructions or registers, all functions
-//! in this module return the bits of the floating point value as `u32` or
-//! `u64` instead of `f32` or `f64`.
+//! in this module take or return the bits of the floating point value as `u32`
+//! or `u64` instead of `f32` or `f64`.
 
 #[inline]
 pub fn u8_to_f32(x: u8) -> u32 {
@@ -152,3 +152,51 @@ impl_signed!(i64_to_f64_round i64 u64_to_f64_round u64);
 impl_signed!(i64_to_f64_truncate i64 u64_to_f64_truncate u64);
 impl_signed!(i128_to_f64_round i128 u128_to_f64_round u64);
 impl_signed!(i128_to_f64_truncate i128 u128_to_f64_truncate u64);
+
+macro_rules! impl_to_int {
+    ($f:tt $s:tt $e:tt $n:tt $t:tt $u:tt $signed:tt) => {
+        #[inline]
+        pub fn $n(mut x: $f) -> $t {
+            const BITS: u32 = $t::MAX.count_ones();
+            const FBITS: u32 = $f::MAX.count_ones();
+            let s = $signed && x >> FBITS - 1 > 0;
+            if s { x &= !0 >> 1; } // Remove sign
+            if x > $e * 2 + 1 << $s { // Negative or ±NaN
+                0
+            } else if x >= $e + (BITS as $f) << $s { // >= max (incl. inf)
+                if s { $t::MIN } else { $t::MAX }
+            } else if x >= $e << $s { // >= 1, < max
+                const R: u32 = if BITS > $s { 0 } else { $s - BITS };
+                const L: u32 = if BITS < $s { 0 } else { BITS - $s };
+                let y = (((x >> R) as $u) << L >> 1 | 1 << BITS - 1) >> $e + BITS - 1 - (x >> $s) as u32;
+                (if s { !y + 1 } else { y }) as $t
+            } else { // >= 0, < 1
+                0
+            }
+        }
+    }
+}
+
+impl_to_int!(u32 23 127 f32_to_u8 u8 u8 false);
+impl_to_int!(u32 23 127 f32_to_u16 u16 u16 false);
+impl_to_int!(u32 23 127 f32_to_u32 u32 u32 false);
+impl_to_int!(u32 23 127 f32_to_u64 u64 u64 false);
+impl_to_int!(u32 23 127 f32_to_u128 u128 u128 false);
+
+impl_to_int!(u32 23 127 f32_to_i8 i8 u8 true);
+impl_to_int!(u32 23 127 f32_to_i16 i16 u16 true);
+impl_to_int!(u32 23 127 f32_to_i32 i32 u32 true);
+impl_to_int!(u32 23 127 f32_to_i64 i64 u64 true);
+impl_to_int!(u32 23 127 f32_to_i128 i128 u128 true);
+
+impl_to_int!(u64 52 1023 f64_to_u8 u8 u8 false);
+impl_to_int!(u64 52 1023 f64_to_u16 u16 u16 false);
+impl_to_int!(u64 52 1023 f64_to_u32 u32 u32 false);
+impl_to_int!(u64 52 1023 f64_to_u64 u64 u64 false);
+impl_to_int!(u64 52 1023 f64_to_u128 u128 u128 false);
+
+impl_to_int!(u64 52 1023 f64_to_i8 i8 u8 true);
+impl_to_int!(u64 52 1023 f64_to_i16 i16 u16 true);
+impl_to_int!(u64 52 1023 f64_to_i32 i32 u32 true);
+impl_to_int!(u64 52 1023 f64_to_i64 i64 u64 true);
+impl_to_int!(u64 52 1023 f64_to_i128 i128 u128 true);
