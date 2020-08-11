07497598dbdee6f395e65edcef85619a0db252d4
Mara Bos <m-ou.se@m-ou.se>
2020-08-12 01:47:12 +0200

    Update docs.

--- src/lib.rs
+++ src/lib.rs
@@ -13,27 +13,40 @@
 //! functions by making use of native floating point instructions where
 //! possible.
 //!
-//! ## Rounding mode
+//! ## Conversion of integers to floating point values
 //!
-//! - Functions named `_round` round the integer to the closest possible floating point number, and breaks ties to even.
-//! - Functions named `_truncate` truncate the result, which means they round towards zero.
-//! - Functions without a rounding mode in their name do not round. These conversions are always lossless.
+//! - Functions named `_round` round the integer to the closest possible
+//!   floating point number, and breaks ties to even.
+//! - Functions named `_truncate` truncate the result, which means they round
+//!   towards zero.
+//! - Functions without a rounding mode in their name do not round. These
+//!   conversions are always lossless.
+//! - The only conversion that can overflow is `u128_to_f32_round`, in which
+//!   case it returns `f32::INFINITY`.
+//!
+//! ## Conversion of floating point values to integers
+//!
+//! - These conversions truncate, which means they round towards zero.
+//! - Values higher than what the integer can represent (including +∞) result
+//!   in the maximum integer value.
+//! - Values lower than what the integer can represent (including -∞) result
+//!   in the minimum integer value.
+//! - `NaN` is converted to zero.
 //!
 //! ## Speed
 //!
 //! For conversions that aren't available natively, the software
-//! implementations in this crate seem to be both faster and
-//! smaller in almost all cases compared to the ones currently
-//! used by `x as f64` or `x as f64` (from the compiler builtins
-//! runtime support library).
+//! implementations in this crate seem to be both faster and smaller in almost
+//! all cases compared to the ones currently used by `x as <type>` (from the
+//! compiler builtins runtime support library).
 //!
 //! ## Work in progress
 //!
 //! This crate is usable, but still incomplete:
 //!
-//! - There's no support for converting *to* integers yet.
 //! - Native conversions are only available on ARM (32- and 64-bit) and x86 (32- and 64-bit).
 //! - The truncating functions do not (yet) use any native floating point instructions.
+//! - The conversions to integers do not (yet) use any native floating point instructions.
 
 // Used to group items together for #[cfg(..)].
 macro_rules! group {
