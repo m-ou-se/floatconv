d20dd32231d5d7d606368491c14919cb8f20e4b2
Mara Bos <m-ou.se@m-ou.se>
2022-05-31 12:50:40 +0200

    Simplify float-to-int conversions.

--- src/soft.rs
+++ src/soft.rs
@@ -155,50 +155,322 @@ pub fn i128_to_f64(i: i128) -> u64 {
     u128_to_f64(i.unsigned_abs()) | sign_bit
 }
 
-macro_rules! impl_to_int {
-    ($f:tt $s:tt $e:tt $n:tt $t:tt $u:tt $signed:tt) => {
-        #[cfg_attr(not(noinline), inline)]
-        pub fn $n(mut x: $f) -> $t {
-            const BITS: u32 = $t::MAX.count_ones();
-            const FBITS: u32 = $f::MAX.count_ones();
-            let s = $signed && x >> FBITS - 1 > 0;
-            if s { x &= !0 >> 1; } // Remove sign
-            if x > $e * 2 + 1 << $s { // Negative or ±NaN
-                0
-            } else if x >= $e + (BITS as $f) << $s { // >= max (incl. inf)
-                if s { $t::MIN } else { $t::MAX }
-            } else if x >= $e << $s { // >= 1, < max
-                const R: u32 = if BITS > $s { 0 } else { $s - BITS };
-                const L: u32 = if BITS < $s { 0 } else { BITS - $s };
-                let y = (((x >> R) as $u) << L >> 1 | 1 << BITS - 1) >> $e + BITS - 1 - (x >> $s) as u32;
-                (if s { !y + 1 } else { y }) as $t
-            } else { // >= 0, < 1
-                0
-            }
-        }
+#[cfg_attr(not(noinline), inline)]
+pub fn f32_to_u8(f: u32) -> u8 {
+    if f < 127 << 23 { // >= 0, < 1
+        0
+    } else if f < 135 << 23 { // >= 1, < max
+        let m = 1 << 7 | (f >> 16) as u8; // Mantissa and the implicit 1-bit.
+        let s = 134 - (f >> 23); // Shift based on the exponent and bias.
+        m >> s
+    } else if f <= 255 << 23 { // >= max (incl. inf)
+        u8::MAX
+    } else { // Negative or NaN
+        0
     }
 }
 
-impl_to_int!(u32 23 127 f32_to_u8 u8 u8 false);
-impl_to_int!(u32 23 127 f32_to_u16 u16 u16 false);
-impl_to_int!(u32 23 127 f32_to_u32 u32 u32 false);
-impl_to_int!(u32 23 127 f32_to_u64 u64 u64 false);
-impl_to_int!(u32 23 127 f32_to_u128 u128 u128 false);
+#[cfg_attr(not(noinline), inline)]
+pub fn f32_to_u16(f: u32) -> u16 {
+    if f < 127 << 23 { // >= 0, < 1
+        0
+    } else if f < 143 << 23 { // >= 1, < max
+        let m = 1 << 15 | (f >> 8) as u16; // Mantissa and the implicit 1-bit.
+        let s = 142 - (f >> 23); // Shift based on the exponent and bias.
+        m >> s
+    } else if f <= 255 << 23 { // >= max (incl. inf)
+        u16::MAX
+    } else { // Negative or NaN
+        0
+    }
+}
 
-impl_to_int!(u32 23 127 f32_to_i8 i8 u8 true);
-impl_to_int!(u32 23 127 f32_to_i16 i16 u16 true);
-impl_to_int!(u32 23 127 f32_to_i32 i32 u32 true);
-impl_to_int!(u32 23 127 f32_to_i64 i64 u64 true);
-impl_to_int!(u32 23 127 f32_to_i128 i128 u128 true);
+#[cfg_attr(not(noinline), inline)]
+pub fn f32_to_u32(f: u32) -> u32 {
+    if f < 127 << 23 { // >= 0, < 1
+        0
+    } else if f < 159 << 23 { // >= 1, < max
+        let m = 1 << 31 | f << 8; // Mantissa and the implicit 1-bit.
+        let s = 158 - (f >> 23); // Shift based on the exponent and bias.
+        m >> s
+    } else if f <= 255 << 23 { // >= max (incl. inf)
+        u32::MAX
+    } else { // Negative or NaN
+        0
+    }
+}
 
-impl_to_int!(u64 52 1023 f64_to_u8 u8 u8 false);
-impl_to_int!(u64 52 1023 f64_to_u16 u16 u16 false);
-impl_to_int!(u64 52 1023 f64_to_u32 u32 u32 false);
-impl_to_int!(u64 52 1023 f64_to_u64 u64 u64 false);
-impl_to_int!(u64 52 1023 f64_to_u128 u128 u128 false);
+#[cfg_attr(not(noinline), inline)]
+pub fn f32_to_u64(f: u32) -> u64 {
+    if f < 127 << 23 { // >= 0, < 1
+        0
+    } else if f < 191 << 23 { // >= 1, < max
+        let m = 1 << 63 | (f as u64) << 40; // Mantissa and the implicit 1-bit.
+        let s = 190 - (f >> 23); // Shift based on the exponent and bias.
+        m >> s
+    } else if f <= 255 << 23 { // >= max (incl. inf)
+        u64::MAX
+    } else { // Negative or NaN
+        0
+    }
+}
 
-impl_to_int!(u64 52 1023 f64_to_i8 i8 u8 true);
-impl_to_int!(u64 52 1023 f64_to_i16 i16 u16 true);
-impl_to_int!(u64 52 1023 f64_to_i32 i32 u32 true);
-impl_to_int!(u64 52 1023 f64_to_i64 i64 u64 true);
-impl_to_int!(u64 52 1023 f64_to_i128 i128 u128 true);
+#[cfg_attr(not(noinline), inline)]
+pub fn f32_to_u128(f: u32) -> u128 {
+    if f < 127 << 23 { // >= 0, < 1
+        0
+    } else if f < 255 << 23 { // >= 1, < inf
+        let m = 1 << 127 | (f as u128) << 104; // Mantissa and the implicit 1-bit.
+        let s = 254 - (f >> 23); // Shift based on the exponent and bias.
+        m >> s
+    } else if f == 255 << 23 { // == inf
+        u128::MAX
+    } else { // Negative or NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f64_to_u8(f: u64) -> u8 {
+    if f < 1023 << 52 { // >= 0, < 1
+        0
+    } else if f < 1031 << 52 { // >= 1, < max
+        let m = 1 << 7 | (f >> 45) as u8; // Mantissa and the implicit 1-bit.
+        let s = 1030 - (f >> 52); // Shift based on the exponent and bias.
+        m >> s
+    } else if f <= 2047 << 52 { // >= max (incl. inf)
+        u8::MAX
+    } else { // Negative or NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f64_to_u16(f: u64) -> u16 {
+    if f < 1023 << 52 { // >= 0, < 1
+        0
+    } else if f < 1039 << 52 { // >= 1, < max
+        let m = 1 << 15 | (f >> 37) as u16; // Mantissa and the implicit 1-bit.
+        let s = 1038 - (f >> 52); // Shift based on the exponent and bias.
+        m >> s
+    } else if f <= 2047 << 52 { // >= max (incl. inf)
+        u16::MAX
+    } else { // Negative or NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f64_to_u32(f: u64) -> u32 {
+    if f < 1023 << 52 { // >= 0, < 1
+        0
+    } else if f < 1055 << 52 { // >= 1, < max
+        let m = 1 << 31 | (f >> 21) as u32; // Mantissa and the implicit 1-bit.
+        let s = 1054 - (f >> 52); // Shift based on the exponent and bias.
+        m >> s
+    } else if f <= 2047 << 52 { // >= max (incl. inf)
+        u32::MAX
+    } else { // Negative or NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f64_to_u64(f: u64) -> u64 {
+    if f < 1023 << 52 { // >= 0, < 1
+        0
+    } else if f < 1087 << 52 { // >= 1, < max
+        let m = 1 << 63 | f << 11; // Mantissa and the implicit 1-bit.
+        let s = 1086 - (f >> 52); // Shift based on the exponent and bias.
+        m >> s
+    } else if f <= 2047 << 52 { // >= max (incl. inf)
+        u64::MAX
+    } else { // Negative or NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f64_to_u128(f: u64) -> u128 {
+    if f < 1023 << 52 { // >= 0, < 1
+        0
+    } else if f < 1151 << 52 { // >= 1, < max
+        let m = 1 << 127 | (f as u128) << 75; // Mantissa and the implicit 1-bit.
+        let s = 1150 - (f >> 52); // Shift based on the exponent and bias.
+        m >> s
+    } else if f <= 2047 << 52 { // >= max (incl. inf)
+        u128::MAX
+    } else { // Negative or NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f32_to_i8(f: u32) -> i8 {
+    let a = f & !0 >> 1; // Remove sign bit.
+    if a < 127 << 23 { // >= 0, < 1
+        0
+    } else if a < 134 << 23 { // >= 1, < max
+        let m = 1 << 7 | (a >> 16) as u8; // Mantissa and the implicit 1-bit.
+        let s = 134 - (a >> 23); // Shift based on the exponent and bias.
+        let u = (m >> s) as i8; // Unsigned result.
+        if (f as i32) < 0 { -u } else { u }
+    } else if a <= 255 << 23 { // >= max (incl. inf)
+        if (f as i32) < 0 { i8::MIN } else { i8::MAX }
+    } else { // NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f32_to_i16(f: u32) -> i16 {
+    let a = f & !0 >> 1; // Remove sign bit.
+    if a < 127 << 23 { // >= 0, < 1
+        0
+    } else if a < 142 << 23 { // >= 1, < max
+        let m = 1 << 15 | (a >> 8) as u16; // Mantissa and the implicit 1-bit.
+        let s = 142 - (a >> 23); // Shift based on the exponent and bias.
+        let u = (m >> s) as i16; // Unsigned result.
+        if (f as i32) < 0 { -u } else { u }
+    } else if a <= 255 << 23 { // >= max (incl. inf)
+        if (f as i32) < 0 { i16::MIN } else { i16::MAX }
+    } else { // NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f32_to_i32(f: u32) -> i32 {
+    let a = f & !0 >> 1; // Remove sign bit.
+    if a < 127 << 23 { // >= 0, < 1
+        0
+    } else if a < 158 << 23 { // >= 1, < max
+        let m = 1 << 31 | a << 8; // Mantissa and the implicit 1-bit.
+        let s = 158 - (a >> 23); // Shift based on the exponent and bias.
+        let u = (m >> s) as i32; // Unsigned result.
+        if (f as i32) < 0 { -u } else { u }
+    } else if a <= 255 << 23 { // >= max (incl. inf)
+        if (f as i32) < 0 { i32::MIN } else { i32::MAX }
+    } else { // NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f32_to_i64(f: u32) -> i64 {
+    let a = f & !0 >> 1; // Remove sign bit.
+    if a < 127 << 23 { // >= 0, < 1
+        0
+    } else if a < 190 << 23 { // >= 1, < max
+        let m = 1 << 63 | (a as u64) << 40; // Mantissa and the implicit 1-bit.
+        let s = 190 - (a >> 23); // Shift based on the exponent and bias.
+        let u = (m >> s) as i64; // Unsigned result.
+        if (f as i32) < 0 { -u } else { u }
+    } else if a <= 255 << 23 { // >= max (incl. inf)
+        if (f as i32) < 0 { i64::MIN } else { i64::MAX }
+    } else { // NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f32_to_i128(f: u32) -> i128 {
+    let a = f & !0 >> 1; // Remove sign bit.
+    if a < 127 << 23 { // >= 0, < 1
+        0
+    } else if a < 254 << 23 { // >= 1, < max
+        let m = 1 << 127 | (a as u128) << 104; // Mantissa and the implicit 1-bit.
+        let s = 254 - (a >> 23); // Shift based on the exponent and bias.
+        let u = (m >> s) as i128; // Unsigned result.
+        if (f as i32) < 0 { -u } else { u }
+    } else if a <= 255 << 23 { // >= max (incl. inf)
+        if (f as i32) < 0 { i128::MIN } else { i128::MAX }
+    } else { // NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f64_to_i8(f: u64) -> i8 {
+    let a = f & !0 >> 1; // Remove sign bit.
+    if a < 1023 << 52 { // >= 0, < 1
+        0
+    } else if a < 1030 << 52 { // >= 1, < max
+        let m = 1 << 7 | (a >> 45) as u8; // Mantissa and the implicit 1-bit.
+        let s = 1030 - (a >> 52); // Shift based on the exponent and bias.
+        let u = (m >> s) as i8; // Unsigned result.
+        if (f as i64) < 0 { -u } else { u }
+    } else if a <= 2047 << 52 { // >= max (incl. inf)
+        if (f as i64) < 0 { i8::MIN } else { i8::MAX }
+    } else { // NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f64_to_i16(f: u64) -> i16 {
+    let a = f & !0 >> 1; // Remove sign bit.
+    if a < 1023 << 52 { // >= 0, < 1
+        0
+    } else if a < 1038 << 52 { // >= 1, < max
+        let m = 1 << 15 | (a >> 37) as u16; // Mantissa and the implicit 1-bit.
+        let s = 1038 - (a >> 52); // Shift based on the exponent and bias.
+        let u = (m >> s) as i16; // Unsigned result.
+        if (f as i64) < 0 { -u } else { u }
+    } else if a <= 2047 << 52 { // >= max (incl. inf)
+        if (f as i64) < 0 { i16::MIN } else { i16::MAX }
+    } else { // NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f64_to_i32(f: u64) -> i32 {
+    let a = f & !0 >> 1; // Remove sign bit.
+    if a < 1023 << 52 { // >= 0, < 1
+        0
+    } else if a < 1054 << 52 { // >= 1, < max
+        let m = 1 << 31 | (a >> 21) as u32; // Mantissa and the implicit 1-bit.
+        let s = 1054 - (a >> 52); // Shift based on the exponent and bias.
+        let u = (m >> s) as i32; // Unsigned result.
+        if (f as i64) < 0 { -u } else { u }
+    } else if a <= 2047 << 52 { // >= max (incl. inf)
+        if (f as i64) < 0 { i32::MIN } else { i32::MAX }
+    } else { // NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f64_to_i64(f: u64) -> i64 {
+    let a = f & !0 >> 1; // Remove sign bit.
+    if a < 1023 << 52 { // >= 0, < 1
+        0
+    } else if a < 1086 << 52 { // >= 1, < max
+        let m = 1 << 63 | a << 11; // Mantissa and the implicit 1-bit.
+        let s = 1086 - (a >> 52); // Shift based on the exponent and bias.
+        let u = (m >> s) as i64; // Unsigned result.
+        if (f as i64) < 0 { -u } else { u }
+    } else if a <= 2047 << 52 { // >= max (incl. inf)
+        if (f as i64) < 0 { i64::MIN } else { i64::MAX }
+    } else { // NaN
+        0
+    }
+}
+
+#[cfg_attr(not(noinline), inline)]
+pub fn f64_to_i128(f: u64) -> i128 {
+    let a = f & !0 >> 1; // Remove sign bit.
+    if a < 1023 << 52 { // >= 0, < 1
+        0
+    } else if a < 1150 << 52 { // >= 1, < max
+        let m = 1 << 127 | (a as u128) << 75; // Mantissa and the implicit 1-bit.
+        let s = 1150 - (a >> 52); // Shift based on the exponent and bias.
+        let u = (m >> s) as i128; // Unsigned result.
+        if (f as i64) < 0 { -u } else { u }
+    } else if a <= 2047 << 52 { // >= max (incl. inf)
+        if (f as i64) < 0 { i128::MIN } else { i128::MAX }
+    } else { // NaN
+        0
+    }
+}
